c6ddee1af69ee7e43594be10a7c47750e6f7b8e7
Vladislav Nepogodin <nepogodin.vlad@gmail.com>
2023-02-19 17:02:07 +0400

    rewrite repo-add in rust

---
+++ scripts/repo-add/src/parse_args.rs
@@ -0,0 +1,86 @@
+#[derive(Debug)]
+pub struct ArgStruct {
+    pub quiet: bool,
+    pub only_add_new: bool,
+    pub rm_existing: bool,
+    pub sign: bool,
+    pub key: bool,
+    pub verify: bool,
+    pub clean_lock: bool,
+    pub use_colors: bool,
+    pub prevent_downgrade: bool,
+
+    pub cmd_line: String,
+
+    pub repo_db_file: Option<String>,
+    pub repo_db_prefix: Option<String>,
+    pub repo_db_suffix: Option<String>,
+    pub lockfile: Option<String>,
+    pub gpgkey: Option<String>,
+}
+
+impl ArgStruct {
+    pub fn new() -> Self {
+        Self {
+            quiet: false,
+            only_add_new: false,
+            rm_existing: false,
+            sign: false,
+            key: false,
+            verify: false,
+            clean_lock: false,
+            use_colors: true,
+            prevent_downgrade: false,
+
+            cmd_line: String::new(),
+
+            repo_db_file: None,
+            repo_db_prefix: None,
+            repo_db_suffix: None,
+            lockfile: None,
+            gpgkey: None,
+        }
+    }
+}
+
+pub fn parse_args<'a>(pargs: &'a [String]) -> (Option<&[String]>, ArgStruct) {
+    let mut argstruct = ArgStruct::new();
+    argstruct.cmd_line = crate::utils::get_current_cmdname(pargs[0].as_str()).to_owned();
+
+    let mut i = 1;
+    while i < pargs.len() {
+        let argument = pargs[i].as_str();
+        if argument == "--quiet" || argument == "-q" {
+            argstruct.quiet = true;
+        } else if argument == "--new" || argument == "-n" {
+            argstruct.only_add_new = true;
+        } else if argument == "--remove" || argument == "-R" {
+            argstruct.rm_existing = true;
+        } else if argument == "--nocolor" {
+            argstruct.use_colors = false;
+        } else if argument == "--sign" || argument == "-s" {
+            argstruct.sign = true;
+        } else if argument == "--key" || argument == "-k" {
+            i += 1;
+            if i < pargs.len() {
+                argstruct.gpgkey = Some(pargs[i].clone());
+                argstruct.key = true;
+            }
+        } else if argument == "--verify" || argument == "-v" {
+            argstruct.verify = true;
+        } else if argument == "--prevent-downgrade" || argument == "-p" {
+            argstruct.prevent_downgrade = true;
+        } else if argument == "--" {
+            i += 1;
+            break;
+        } else {
+            break;
+        }
+        i += 1;
+    }
+    if i >= pargs.len() {
+        return (None, argstruct);
+    }
+
+    (pargs.get(i..), argstruct)
+}
---
+++ scripts/repo-add/src/pkginfo.rs
@@ -0,0 +1,180 @@
+use std::fs;
+use std::fs::File;
+
+use akv::reader::ArchiveReader;
+use log;
+
+#[derive(Debug)]
+pub struct PkgInfo {
+    pub pkgname: Option<String>,
+    pub pkgbase: Option<String>,
+    pub pkgver: Option<String>,
+    pub basever: Option<String>,
+    pub pkgdesc: Option<String>,
+    pub url: Option<String>,
+    pub builddate: Option<String>,
+    pub packager: Option<String>,
+    pub arch: Option<String>,
+    pub pkg_isize: Option<String>,
+
+    pub groups: Vec<String>,
+    pub licenses: Vec<String>,
+    pub depends: Vec<String>,
+    pub optdepends: Vec<String>,
+    pub makedepends: Vec<String>,
+    pub checkdepends: Vec<String>,
+    pub conflicts: Vec<String>,
+    pub replaces: Vec<String>,
+    pub provides: Vec<String>,
+    pub backup: Vec<String>,
+}
+
+impl PkgInfo {
+    pub fn new() -> Self {
+        Self {
+            pkgname: None,
+            pkgbase: None,
+            pkgver: None,
+            basever: None,
+            pkgdesc: None,
+            url: None,
+            builddate: None,
+            packager: None,
+            arch: None,
+            pkg_isize: None,
+
+            groups: vec![],
+            licenses: vec![],
+            depends: vec![],
+            optdepends: vec![],
+            makedepends: vec![],
+            checkdepends: vec![],
+            conflicts: vec![],
+            replaces: vec![],
+            provides: vec![],
+            backup: vec![],
+        }
+    }
+
+    pub fn parse_line(self: &mut Self, line: &str) -> bool {
+        let split_line = line.split('=').collect::<Vec<&str>>();
+
+        // Just drop the line, if the line is not pair separated by '='.
+        if split_line.len() != 2 {
+            return false;
+        }
+
+        let key = split_line[0].trim();
+        let value = split_line[1].trim();
+
+        let mut res_status = true;
+        match key {
+            "pkgname" => self.pkgname = Some(value.to_owned()),
+            "pkgbase" => self.pkgbase = Some(value.to_owned()),
+            "pkgver" => self.pkgver = Some(value.to_owned()),
+            "basever" => self.basever = Some(value.to_owned()),
+            "pkgdesc" => self.pkgdesc = Some(value.to_owned()),
+            "group" => self.groups.push(value.to_owned()),
+            "url" => self.url = Some(value.to_owned()),
+            "license" => self.licenses.push(value.to_owned()),
+            "builddate" => self.builddate = Some(value.to_owned()),
+            "packager" => self.packager = Some(value.to_owned()),
+            "arch" => self.arch = Some(value.to_owned()),
+            "size" => self.pkg_isize = Some(value.to_owned()),
+            "depend" => self.depends.push(value.to_owned()),
+            "optdepend" => self.optdepends.push(value.to_owned()),
+            "makedepend" => self.makedepends.push(value.to_owned()),
+            "checkdepend" => self.checkdepends.push(value.to_owned()),
+            "conflict" => self.conflicts.push(value.to_owned()),
+            "replaces" => self.replaces.push(value.to_owned()),
+            "provides" => self.provides.push(value.to_owned()),
+            "backup" => self.backup.push(value.to_owned()),
+            _ => res_status = false,
+        };
+
+        res_status
+    }
+
+    pub fn from_string(content: &String) -> Self {
+        let mut pkginfo = PkgInfo::new();
+
+        let lines =
+            content.lines().into_iter().filter(|line| !(line.is_empty() || line.starts_with('#')));
+        for line in lines {
+            pkginfo.parse_line(line);
+        }
+
+        pkginfo
+    }
+
+    pub fn from_file(file_path: &str) -> Self {
+        let file_content = fs::read_to_string(file_path);
+        if file_content.is_err() {
+            return PkgInfo::new();
+        }
+        PkgInfo::from_string(&file_content.unwrap())
+    }
+
+    pub fn from_archive(file_path: &str) -> Self {
+        let file_archive = File::open(file_path);
+        if file_archive.is_err() {
+            log::error!("could not open file {}: {:?}", file_path, file_archive.err());
+            return PkgInfo::new();
+        }
+        let file_metadata = fs::metadata(file_path);
+        let pkg_isize = format!("{}", file_metadata.unwrap().len());
+
+        let mut pkginfo = PkgInfo::new();
+        pkginfo.pkg_isize = Some(pkg_isize);
+
+        let mut archive_reader = ArchiveReader::open_io(file_archive.unwrap());
+        if archive_reader.is_err() {
+            log::error!("error while reading package  {}: {:?}\n", file_path, archive_reader.err());
+            return PkgInfo::new();
+        }
+        while let Some(entry) = archive_reader.as_mut().unwrap().next_entry().unwrap() {
+            if entry.pathname_utf8().unwrap() != ".PKGINFO" {
+                continue;
+            }
+            let entry_reader = entry.into_reader();
+            let entry_content = std::io::read_to_string(entry_reader).unwrap();
+            for content_line in entry_content.lines() {
+                pkginfo.parse_line(content_line);
+            }
+
+            break;
+        }
+
+        pkginfo
+    }
+}
+
+pub fn list_archive(file_path: &str) -> Vec<String> {
+    let file_archive = File::open(file_path);
+    if file_archive.is_err() {
+        log::error!("could not open file {}: {:?}", file_path, file_archive.err());
+        return vec![];
+    }
+
+    let mut arc_files = vec![];
+
+    let mut archive_reader = ArchiveReader::open_io(file_archive.unwrap());
+    if archive_reader.is_err() {
+        log::error!("error while reading package  {}: {:?}\n", file_path, archive_reader.err());
+        return vec![];
+    }
+    while let Some(entry) = archive_reader.as_mut().unwrap().next_entry().unwrap() {
+        let entry_path = entry.pathname_mb();
+
+        // Ignore if package entry has broken path (that is failed to convert to utf8), or if the
+        // entry starts with '.'.
+        if entry_path.is_err()
+            || entry_path.as_ref().unwrap().to_str().unwrap().to_owned().starts_with('.')
+        {
+            continue;
+        }
+        arc_files.push(entry_path.unwrap().to_str().unwrap().to_owned());
+    }
+
+    arc_files
+}
