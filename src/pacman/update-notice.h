51bec5d07de54b014a4ba7ccb0e0bd822bdb50a7
Vladislav Nepogodin <nepogodin.vlad@gmail.com>
2023-05-12 03:47:21 +0400

    add update message/notice
    This adds update notices.
    Message is fetch from our endpoint.

---
+++ src/pacman/update-notice.h
@@ -0,0 +1,26 @@
+/*
+ *  update-notice.h
+ *
+ *  Copyright (c) 2023 by Vladislav Nepogodin <vnepogodin@cachyos.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef PM_UPDATE_NOTICE_H
+#define PM_UPDATE_NOTICE_H
+
+#include <stdlib.h>
+
+int do_update_notice(void);
+
+#endif /* PM_UPDATE_NOTICE_H */
