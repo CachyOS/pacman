9fe3bf54a1224bfcffcf0757b7edfe800618b01d
Vladislav Nepogodin <nepogodin.vlad@gmail.com>
2023-05-12 03:47:21 +0400

    add update message/notice
    This adds update notices.
    Message is fetch from our endpoint.

--- src/pacman/sync.c
+++ src/pacman/sync.c
@@ -37,6 +37,7 @@
 #include "package.h"
 #include "callback.h"
 #include "conf.h"
+#include "update-notice.h"
 
 static int unlink_verbose(const char *pathname, int ignore_missing)
 {
@@ -718,6 +719,11 @@ static int sync_trans(alpm_list_t *targets)
 			alpm_logaction(config->handle, PACMAN_CALLER_PREFIX,
 					"starting full system upgrade\n");
 		}
+		if (do_update_notice() == -1) {
+			trans_release();
+			return 1;
+		}
+
 		if(alpm_sync_sysupgrade(config->handle, config->op_s_upgrade >= 2) == -1) {
 			pm_printf(ALPM_LOG_ERROR, "%s\n", alpm_strerror(alpm_errno(config->handle)));
 			trans_release();
