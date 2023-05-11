88de97e6b0d989206fc68f83350897b8467d01b0
Vladislav Nepogodin <nepogodin.vlad@gmail.com>
2023-05-12 03:47:21 +0400

    add update message/notice
    This adds update notices.
    Message is fetch from our endpoint.

---
+++ src/pacman/update-notice.c
@@ -0,0 +1,161 @@
+/*
+ *  update-notice.c
+ *
+ *  Copyright (c) 2023 by Vladislav Nepogodin <vnepogodin@cachyos.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h> /* intmax_t */
+#include <string.h>
+#ifdef HAVE_LIBCURL
+#include <curl/curl.h>
+#endif
+
+#include <alpm.h>
+#include <alpm_list.h>
+
+#define JSMN_STATIC
+#include "jsmn.h"
+
+/* pacman */
+#include "update-notice.h"
+#include "util.h"
+#include "conf.h"
+
+struct url_data {
+    size_t size;
+    char* data;
+};
+
+static size_t write_data(void *ptr, size_t size, size_t nmemb, struct url_data *data) {
+    size_t index = data->size;
+    size_t n = (size * nmemb);
+
+    data->size += (size * nmemb);
+
+    char* tmp = realloc(data->data, data->size + 1); /* +1 for '\0' */
+    if (tmp) {
+        data->data = tmp;
+    } else {
+        if(data->data) {
+            free(data->data);
+        }
+        return 0;
+    }
+
+    memcpy((data->data + index), ptr, n);
+    data->data[data->size] = '\0';
+
+    return size * nmemb;
+}
+
+static char *handle_url(const char* url) {
+    struct url_data data;
+    data.size = 0;
+    data.data = malloc(4096); /* reasonable size initial buffer */
+    if (data.data == NULL) {
+        return NULL;
+    }
+
+    data.data[0] = '\0';
+
+    CURL *curl = curl_easy_init();
+    if (curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, url);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &data);
+        CURLcode res = curl_easy_perform(curl);
+        if(res != CURLE_OK) {
+			free(data.data);
+			return NULL;
+        }
+
+        curl_easy_cleanup(curl);
+
+    }
+    return data.data;
+}
+
+static int jsoneq(const char *json, jsmntok_t *tok, const char *s) {
+  if (tok->type == JSMN_STRING && (int)strlen(s) == tok->end - tok->start &&
+      strncmp(json + tok->start, s, tok->end - tok->start) == 0) {
+    return 0;
+  }
+  return -1;
+}
+
+static int check_cachyos_repos(void) {
+	alpm_list_t* dbs = alpm_get_syncdbs(config->handle);
+	for (alpm_list_t* i = dbs; i; i = i->next) {
+		const char* db_name = alpm_db_get_name(i->data);
+		if(strstr(db_name, "cachyos") != NULL) {
+			return 0;
+		}
+	}
+
+	return -1;
+}
+
+static char* get_body_from_json(char* data) {
+	jsmn_parser p;
+	jsmntok_t t[128]; /* We expect no more than 4 tokens */
+
+	jsmn_init(&p);
+	int ret = jsmn_parse(&p, data, strlen(data), t, sizeof(t) / sizeof(t[0]));
+	if (ret < 0) {
+		return NULL;
+	}
+
+	/* Assume the top-level element is an object */
+	if (ret < 1 || t[0].type != JSMN_OBJECT) {
+		return NULL;
+	}
+
+	/* Loop over all keys of the root object */
+	for (int i = 1; i < ret; ++i) {
+		if (jsoneq(data, &t[i], "body") == 0) {
+			return strndup(data + t[i + 1].start, t[i + 1].end - t[i + 1].start);
+		}
+	}
+
+	return NULL;
+}
+
+int do_update_notice(void) {
+	/* check if we have cachyos repositories */
+	int ret = check_cachyos_repos();
+	if (ret != 0) {
+		return 0;
+	}
+
+	char* data = handle_url("https://iso-stats.cachyos.org/api/v2/last_update_notice");
+	if (data) {
+		char* body_msg = get_body_from_json(data);
+		if (body_msg) {
+			printf("%sUpdate notice: %s%s\n", config->colstr.warn,
+								config->colstr.nocolor, body_msg);
+			if (yesno(_("Do you want to continue?")) == 0) {
+				ret = -1;
+			}
+
+			free(body_msg);
+		}
+        free(data);
+	}
+
+	return ret;
+}
